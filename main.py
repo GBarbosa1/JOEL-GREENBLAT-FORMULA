ae5af147ff9d4e735af16e9754ee56737ccbd1e5
GBarbosa1 <barbosa.gbsantos@gmail.com>
2023-02-05 12:17:45 -0300

    Added the trailing pe data Capped the original json file to improve developing.

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ import yfinance as yf
 import pandas as pd
 
 assets_dataframe = pd.read_json('assets_name.json')
-print(assets_dataframe)
+roa_list = []
+pe_list = []
 
 
 for index, row in assets_dataframe.iterrows():
     asset = row['YFINANCE_CODE']
-    asset_data = yf.Ticker(asset).info
+    
+    
     try:
+        asset_data = yf.Ticker(asset).info
         asset_roa = 100*asset_data['returnOnAssets']
+        asset_trailing_pe = asset_data['trailingPE']
+        asset_roa = round(asset_roa,2)
+        asset_trailing_pe = round(asset_trailing_pe,2)
+        roa_list.append(asset_roa)
+        pe_list.append(asset_trailing_pe)
+
+    
 
     except:
         print(asset+'error')
-    print(asset_roa)
 
+print(roa_list)
 
